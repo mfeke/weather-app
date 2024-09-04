909d0a40cb2586c3a5d744f4d630e5b8539d8a62
mfeke <mfekethohulane@gail.com>
2024-09-04 13:51:23 +0200

    update

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './components/home/home.component';
+import { DemoComponent } from './components/demo/demo.component';
 
 const routes: Routes = [
-  {path:"home", component:HomeComponent}
+  {path:"home", component:HomeComponent},
+  { path: '',   redirectTo: '/home', pathMatch: 'full' }, // redirect to `first-component`
+{ path: '**', component: DemoComponent },  // Wildcard route for a 404 page
+
 ];
 
 @NgModule({
